9a6dffb70f64d6a942c2825ae6ecce2ebbdc4947
arnoldshek <ashek@hawaii.edu>
2018-02-04 23:29:55 -1000

    Submission Commit

---
+++ sumofnumbers.js
@@ -0,0 +1,42 @@
+
+
+const testnumbers = [4, 5, 6, 7];
+
+
+function sumFor(nums) {
+  let total = 0;
+
+  for (const num of nums) {
+    total += num;
+  }
+  return total;
+}
+
+console.log(sumFor(testnumbers));
+
+function sumWhile(nums) {
+  let total = 0;
+  let i = 0;
+  while (i < nums.length) {
+    total += nums[i];
+    i++;
+  }
+  return total;
+}
+
+console.log(sumWhile(testnumbers));
+
+function sumRecursion(nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+  return nums[0] + sumRecursion(nums.slice(1, nums.length));
+}
+
+console.log(sumRecursion(testnumbers));
+
+function sumTheSimpleWay(nums) {
+  return _.reduce(nums, function(memo, num) { return memo + num; }, 0);
+}
+
+console.log(sumTheSimpleWay(testnumbers));
